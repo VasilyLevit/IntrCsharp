7cec48d6c22288b94f319ae0ae1b31a386d66ff4
Vasily Levit <vasily.levit@gmail.com>
2022-07-28 23:17:21 +0800

    add input data in cod

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// See README.md for more information
+
+string[] inArray = {"hello", "2", "world", ":-)"}; // Имеющийся массив строк
+int sizeInArray = inArray.Length;
+string[] outArray = new string[sizeInArray];
+int n = 3; // Максимальное число символов в строке
+int i = 0, j = 0;
+
