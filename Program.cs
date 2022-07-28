d0c62d6aabf09350fe1f7d9aa86bff042c2d6640
Vasily Levit <vasily.levit@gmail.com>
2022-07-29 00:24:38 +0800

    add main cod

--- Program.cs
+++ Program.cs
@@ -1,8 +1,17 @@
 ﻿// See README.md for more information
 
 string[] inArray = {"hello", "2", "world", ":-)"}; // Имеющийся массив строк
+int n = 3; // Максимальное число символов в строке
+
 int sizeInArray = inArray.Length;
 string[] outArray = new string[sizeInArray];
-int n = 3; // Максимальное число символов в строке
-int i = 0, j = 0;
 
+for (int i = 0, j = 0; i < sizeInArray; i++)
+{
+    if (inArray[i].Length <= n)
+    {
+        outArray[j] = inArray[i];
+        Console.Write($"{outArray[j]} "); // Тест
+        j++;
+    }
+}
